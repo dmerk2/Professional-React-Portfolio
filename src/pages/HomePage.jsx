1b7af358dd4848367cdbc8df96c477efac724a23
dmerk2 <dan.merkin@gmail.com>
2023-11-18 11:11:33 -0500

    Creating one homepage to pull all components

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,23 @@
+import MyJumbotron from "../components/MyJumbotron";
+import MyNav from "../components/MyNav";
+import Contact from "../components/Contact";
+import Skills from "../components/Skills";
+import Footer from "../components/Footer";
+import About from "../components/About";
+import Portfolio from "../components/Portfolio";
+
+function HomePage() {
+  return (
+    <div>
+      <MyNav />
+      <MyJumbotron />
+      <About />
+      <Skills />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
