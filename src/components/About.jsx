00afadb9ba7b268ab6b1a3752af9c3c9467d9d06
dmerk2 <dan.merkin@gmail.com>
2023-11-18 14:06:13 -0500

    Refactoring to bootstrap components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,34 +1,36 @@
+import { Container, Row, Col, Image } from "react-bootstrap";
 import profilePic from "../assets/profile-image.png";
 import "../styles/About.css";
 
 const About = () => {
   return (
-    <>
-      <div className="container" id="about">
-        <h2 className="mt-3">About Me</h2>
-        <div className="row align-items-center">
-          <div className="col-md-6 text-center">
-            <img src={profilePic} alt="Daniel Merkin" className="profile-picture"/>
-          </div>
-          <div className="col-md-6">
-            <p>
-              I am a dedicated web developer with a genuine passion for
-              problem-solving and a profound curiosity about how technology
-              shapes our world. In collaborative environments, I thrive, viewing
-              each project as a canvas for creativity and innovation.
-            </p>
-            <p>
-              My journey in technology has been an exciting exploration of
-              emerging trends. I find joy in staying abreast of the latest
-              technologies and eagerly embracing upcoming innovations.
-              Throughout my 34 years, I have come to appreciate the profound
-              impact even small actions can have in making the world a better
-              place.
-            </p>
-          </div>
-        </div>
-      </div>
-    </>
+    <Container id="about" className="about-section">
+      <h2>About Me</h2>
+      <Row className="align-items-center">
+        <Col md={6} className="text-center">
+          <Image
+            src={profilePic}
+            alt="Daniel Merkin"
+            className="profile-picture"
+          />
+        </Col>
+        <Col md={6}>
+          <p>
+            I am a dedicated web developer with a genuine passion for
+            problem-solving and a profound curiosity about how technology shapes
+            our world. In collaborative environments, I thrive, viewing each
+            project as a canvas for creativity and innovation.
+          </p>
+          <p>
+            My journey in technology has been an exciting exploration of
+            emerging trends. I find joy in staying abreast of the latest
+            technologies and eagerly embracing upcoming innovations. Throughout
+            my 34 years, I have come to appreciate the profound impact even
+            small actions can have in making the world a better place.
+          </p>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
